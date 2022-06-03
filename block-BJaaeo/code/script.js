d81ae20aba6aa6612dff4d79167cfbda66c5f1de
jopoepl <joel_joseph93@outlook.com>
2022-06-03 12:55:20 +0530

    Assignment: Calculator

---
+++ block-BJaaeo/code/script.js
@@ -0,0 +1,27 @@
+let buttons = document.querySelector(`.btn-cont`)
+let input = document.querySelector(`input`)
+let calc = document.querySelector(`.container`)
+let screen = document.querySelector(`.screen`)
+
+
+let inputValue = "";
+let result = 0;
+
+calc.addEventListener("click", function(event) { 
+    if(event.target.innerText === `C`){
+        inputValue = "";
+
+    } if(event.target.innerText === `=`){
+        input.value =  result;
+    } else {
+    inputValue += `${event.target.dataset.value}`;
+    input.value = inputValue
+    result = eval(inputValue);   
+    }
+})
+
+
+
+
+
+
