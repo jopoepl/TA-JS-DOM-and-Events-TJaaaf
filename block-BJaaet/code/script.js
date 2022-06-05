d8b4a4838956f74241343899220ca7485ba1671a
jopoepl <joel_joseph93@outlook.com>
2022-06-05 10:23:15 +0530

    Assignment: Watch List-update

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,35 +1,39 @@
-let form = document.querySelector(`form`)
+let todo = document.querySelector(`.todo`)
 let movieInput = document.querySelector(`.movie`)
 let cancel = document.querySelector(`span`)
+label =  document.createElement(`label`)
 
 
 
 
 function addMovie (event) {
-    event.preventDefault();
     let formcontrol = document.createElement(`div`)
     formcontrol.classList.add(`formcontrol`)
     let input = document.createElement(`input`)
     input.type = "checkbox"
-    label =  document.createElement(`label`)
     label.for = "event.target.elements.movie.value"
     label.innerText = movieInput.value
     span = document.createElement(`span`)
     span.innerText = "❌"
     if (event.key === "Enter") {
     formcontrol.append(input, label, span)
-    form.append(formcontrol)
+    todo.append(formcontrol)
     }
 }
+
+
+
+
 
 
 movieInput.addEventListener("keypress", addMovie)
 
 
-form.addEventListener("click", function (event) {
+todo.addEventListener("click", function (event) {
 
 
     if (event.target.localName === "span") {
     event.target.parentElement.remove()
     }
 })
+
